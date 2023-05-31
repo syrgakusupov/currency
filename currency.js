0293638eb2a6dc5859c4391b47af7841215694b9
jkojobekov <jkojobekov@it.gov.kg>
2023-05-31 16:36:10 +0600

    show currency

--- currency.js
+++ currency.js
@@ -1,6 +1,7 @@
 function getUrl(start = 0) {
-    return 'https://api.coinlore.com/api/tickers/?start=' +  start + '&limit=10';
+    return 'https://api.coinlore.com/api/tickers/?start=' + start + '&limit=10';
 }
+
 function getData(url) {
     fetch(url)
         .then(response => response.json())
@@ -22,31 +23,31 @@ function loadDataIntoTable(data) {
         coinSymbol.push(coin.symbol);
         coinPrice.push(coin.price_usd);
         coin24Change.push(coin.percent_change_24h);
-      coin7Change.push(coin.percent_change_7d);
+        coin7Change.push(coin.percent_change_7d);
     });
 
     let tableBody = document.getElementById('crypto-table-body');
 
     let html = "";
 
-    for(let i = 0; i < coinName.length; i++) {
-        html += "<tr> <hr/>" ;
+    for (let i = 0; i < coinName.length; i++) {
+        html += "<tr> <hr/>";
         html += "<td class='rank-text text-darken-4'>" + coinRank[i] + "</td>";
-        html += "<td class='name-text text-darken-4'>" + coinName[i] + " ("  + coinSymbol[i] + ")" + "</td>";
+        html += "<td class='name-text text-darken-4'>" + coinName[i] + " (" + coinSymbol[i] + ")" + "</td>";
         html += "<td class='price-text text-darken-4'>$" + coinPrice[i] + "</td>";
         if (coin24Change[i] > 0) {
             html += "<td class='green-text text-darken-4'>+" + coin24Change[i] + "%</td>";
         } else {
             html += "<td class='red-text text-darken-4'>" + coin24Change[i] + "%</td>";
         }
-          
-          
-          if (coin7Change[i] > 0) {
+
+
+        if (coin7Change[i] > 0) {
             html += "<td class='green-text text-darken-4'>+" + coin7Change[i] + "%</td>";
         } else {
             html += "<td class='red-text text-darken-4'>" + coin7Change[i] + "%</td>";
         }
-        
+
         html += "</tr>";
     }
 
@@ -59,7 +60,7 @@ function handleNumberClick(clickedLink, leftArrow, rightArrow) {
     const url = getUrl((clickedLinkPageNumber * 10) - 10);
     getData(url);
 
-    switch(clickedLinkPageNumber) {
+    switch (clickedLinkPageNumber) {
         case 1:
             disableLeftArrow(leftArrow);
             if (rightArrow.className.indexOf('disabled') !== -1) {
@@ -84,11 +85,11 @@ function handleNumberClick(clickedLink, leftArrow, rightArrow) {
 }
 
 function handleLeftArrowClick(activePageNumber, leftArrow, rightArrow) {
-    let previousPage = document.querySelectorAll('li')[activePageNumber-1];
+    let previousPage = document.querySelectorAll('li')[activePageNumber - 1];
     previousPage.classList = "active";
-    url = getUrl(((activePageNumber-1) * 10) - 10);
+    url = getUrl(((activePageNumber - 1) * 10) - 10);
     getData(url);
-    
+
     if (activePageNumber === 10) {
         enableRightArrow(rightArrow);
     }
@@ -99,10 +100,10 @@ function handleLeftArrowClick(activePageNumber, leftArrow, rightArrow) {
 }
 
 function handleRightArrowClick(activePageNumber, leftArrow, rightArrow) {
-    let nextPage = document.querySelectorAll('li')[activePageNumber+1];
+    let nextPage = document.querySelectorAll('li')[activePageNumber + 1];
     nextPage.classList = "active";
 
-    url = getUrl(((activePageNumber+1) * 10) - 10);
+    url = getUrl(((activePageNumber + 1) * 10) - 10);
     getData(url);
 
     if (activePageNumber === 1) {
@@ -145,7 +146,7 @@ let rightArrow;
 let url = '';
 
 pageLinks.forEach((element) => {
-    element.addEventListener("click", function() {
+    element.addEventListener("click", function () {
         leftArrow = document.querySelector('.arrow-left');
         rightArrow = document.querySelector('.arrow-right');
         console.log(rightArrow);
@@ -170,8 +171,6 @@ pageLinks.forEach((element) => {
     });
 });
 
-
-
 const input_currency = document.querySelector('#input_currency');
 const output_currency = document.querySelector('#output_currency');
 const input_amount = document.querySelector('#input_amount');
@@ -185,24 +184,54 @@ input_amount.addEventListener('input', compute);
 output_amount.addEventListener('input', compute);
 
 
-
-function compute(){
+function compute() {
     const input_currency1 = input_currency.value;
     const output_currency1 = output_currency.value;
 
     fetch(`https://api.exchangerate-api.com/v4/latest/${input_currency1}`)
-    .then(res => res.json())
-    .then(res => {
-        console.log(res);
-        const new_rate = res.rates[output_currency1];
-        rate.innerText = `1 ${input_currency1} =  ${new_rate} ${output_currency1}`
-        output_amount.value = (input_amount.value * new_rate).toFixed(2);
-        console.log(res);
-    })
+        .then(res => res.json())
+        .then(res => {
+            // console.log(res);
+            const new_rate = res.rates[output_currency1];
+            rate.innerText = `1 ${input_currency1} =  ${new_rate} ${output_currency1}`
+            output_amount.value = (input_amount.value * new_rate).toFixed(2);
+            // console.log(res);
+        })
 }
-
-
 
 compute();
 
+/**Запрос для получения валюты * */
 
+document.addEventListener('DOMContentLoaded', async function () {
+    const response = await fetch('https://v6.exchangerate-api.com/v6/a9ee452403364fa939969ff3/latest/USD')
+    const data = await response.json()
+    return showData(data)
+});
+
+document.addEventListener('DOMContentLoaded', async function () {
+    const response = await fetch('https://v6.exchangerate-api.com/v6/a9ee452403364fa939969ff3/latest/RUB')
+    const data = await response.json()
+    return showRub(data)
+});
+
+const showData = (data) => {
+    let display = document.getElementById('description-currency')
+    let show = ''
+
+    show += `<td class='rank-text text-darken-4'>${data.base_code}</td>`
+    show += `<td class='rank-text text-darken-4'>${data.conversion_rates.AED}</td>`
+    show += `<td class='rank-text text-darken-4'>${data.conversion_rates.USD}</td>`
+
+    display.innerHTML = show
+}
+
+const showRub = (data) => {
+    let display = document.getElementById('description-currencyRub')
+    let show = ''
+
+    show += `<td class='rank-text text-darken-4'>${data.base_code}</td>`
+    show += `<td class='rank-text text-darken-4'>${data.conversion_rates.AED}</td>`
+    show += `<td class='rank-text text-darken-4'>${data.conversion_rates.USD}</td>`
+    display.innerHTML = show
+}
